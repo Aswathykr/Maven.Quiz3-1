333a109c811005f410ed08f721ef7eaf94df95f0
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-03-25 10:36:46 -0400

    Fundamental implemented and tested

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -5,6 +5,21 @@ package rocks.zipcode.io.quiz3.fundamentals;
  */
 public class PigLatinGenerator {
     public String translate(String str) {
-        return null;
+        String[] words = str.split(" ");
+        String result = "";
+        for(String word : words) {
+            if (VowelUtils.startsWithVowel(word)) {
+                result += word + "way" + " ";
+            }
+            else{
+                int startIndex = VowelUtils.getIndexOfFirstVowel(word);
+                if(startIndex != -1){
+                    result += word.substring(startIndex) + word.substring(0, startIndex) + "ay" + " ";
+                }else{
+                    result += word + "ay" + " ";
+                }
+            }
+        }
+        return result.substring(0, result.length()-1);
     }
 }
